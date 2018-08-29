de3bed1c633e7f13fd37d03b0b2b92a60fe0e045
selvagsz <selvaganeshbsg@gmail.com>
2018-08-29 20:10:49 +0800

    dedecorate addon/components/ember-td/component.js

--- addon/components/ember-td/component.js
+++ addon/components/ember-td/component.js
@@ -1,11 +1,11 @@
 import BaseTableCell from '../-private/base-table-cell';
 
-import { action, computed } from '@ember-decorators/object';
-import { alias, readOnly } from '@ember-decorators/object/computed';
-import { tagName } from '@ember-decorators/component';
-import { argument } from '@ember-decorators/argument';
-import { type, optional } from '@ember-decorators/argument/type';
-import { Action } from '@ember-decorators/argument/types';
+import { computed } from '@ember/object';
+import { alias, readOnly } from '@ember/object/computed';
+// import { tagName } from '@ember-decorators/component';
+// import { argument } from '@ember-decorators/argument';
+// import { type, optional } from '@ember-decorators/argument/type';
+// import { Action } from '@ember-decorators/argument/types';
 
 import layout from './template';
 import { SELECT_MODE } from '../../-private/collapse-tree';
@@ -37,122 +37,117 @@ import { SELECT_MODE } from '../../-private/collapse-tree';
   @yield {object} columnMeta - The meta object associated with the column
   @yield {object} rowMeta - The meta object associated with the row
 */
-@tagName('td')
-export default class EmberTd extends BaseTableCell {
-  layout = layout;
+export default BaseTableCell.extend({
+  tagName: 'td',
+  layout,
 
   /**
     The API object passed in by the table row
   */
-  @argument
-  @type('object')
-  api;
+  // @argument
+  // @type('object')
+  // api;
 
   /**
     Action sent when the user clicks this element
   */
-  @argument
-  @type(optional(Action))
-  onClick;
+  // @argument
+  // @type(optional(Action))
+  // onClick;
 
   /**
     Action sent when the user double clicks this element
   */
-  @argument
-  @type(optional(Action))
-  onDoubleClick;
+  // @argument
+  // @type(optional(Action))
+  // onDoubleClick;
 
-  @computed('api') // only watch `api` due to a bug in Ember
-  get unwrappedApi() {
-    return this.get('api.api') || this.get('api');
-  }
+  unwrappedApi: computed('api', {
+    // only watch `api` due to a bug in Ember
+    get() {
+      return this.get('api.api') || this.get('api');
+    },
+  }),
 
-  @alias('unwrappedApi.cellValue')
-  cellValue;
+  cellValue: alias('unwrappedApi.cellValue'),
 
-  @readOnly('unwrappedApi.cellMeta')
-  cellMeta;
+  cellMeta: readOnly('unwrappedApi.cellMeta'),
 
-  @readOnly('unwrappedApi.columnValue')
-  columnValue;
+  columnValue: readOnly('unwrappedApi.columnValue'),
 
-  @readOnly('unwrappedApi.columnMeta')
-  columnMeta;
+  columnMeta: readOnly('unwrappedApi.columnMeta'),
 
-  @readOnly('unwrappedApi.rowValue')
-  rowValue;
+  rowValue: readOnly('unwrappedApi.rowValue'),
 
-  @readOnly('unwrappedApi.rowMeta')
-  rowMeta;
+  rowMeta: readOnly('unwrappedApi.rowMeta'),
 
-  @readOnly('unwrappedApi.rowSelectionMode')
-  rowSelectionMode;
+  rowSelectionMode: readOnly('unwrappedApi.rowSelectionMode'),
 
-  @readOnly('unwrappedApi.checkboxSelectionMode')
-  checkboxSelectionMode;
+  checkboxSelectionMode: readOnly('unwrappedApi.checkboxSelectionMode'),
 
-  @readOnly('rowMeta.canCollapse')
-  canCollapse;
+  canCollapse: readOnly('rowMeta.canCollapse'),
 
-  @computed('rowMeta.depth')
-  get depthClass() {
-    return `depth-${this.get('rowMeta.depth')}`;
-  }
+  depthClass: computed('rowMeta.depth', {
+    get() {
+      return `depth-${this.get('rowMeta.depth')}`;
+    },
+  }),
 
-  @computed('shouldShowCheckbox', 'rowSelectionMode')
-  get canSelect() {
-    let rowSelectionMode = this.get('rowSelectionMode');
-    let shouldShowCheckbox = this.get('shouldShowCheckbox');
+  canSelect: computed('shouldShowCheckbox', 'rowSelectionMode', {
+    get() {
+      let rowSelectionMode = this.get('rowSelectionMode');
+      let shouldShowCheckbox = this.get('shouldShowCheckbox');
 
-    return (
-      shouldShowCheckbox ||
-      rowSelectionMode === SELECT_MODE.MULTIPLE ||
-      rowSelectionMode === SELECT_MODE.SINGLE
-    );
-  }
+      return (
+        shouldShowCheckbox ||
+        rowSelectionMode === SELECT_MODE.MULTIPLE ||
+        rowSelectionMode === SELECT_MODE.SINGLE
+      );
+    },
+  }),
 
-  @computed('checkboxSelectionMode')
-  get shouldShowCheckbox() {
-    let checkboxSelectionMode = this.get('checkboxSelectionMode');
+  shouldShowCheckbox: computed('checkboxSelectionMode', {
+    get() {
+      let checkboxSelectionMode = this.get('checkboxSelectionMode');
 
-    return (
-      checkboxSelectionMode === SELECT_MODE.MULTIPLE || checkboxSelectionMode === SELECT_MODE.SINGLE
-    );
-  }
+      return (
+        checkboxSelectionMode === SELECT_MODE.MULTIPLE ||
+        checkboxSelectionMode === SELECT_MODE.SINGLE
+      );
+    },
+  }),
 
-  @action
-  onSelectionToggled(event) {
-    let rowMeta = this.get('rowMeta');
-    let checkboxSelectionMode = this.get('checkboxSelectionMode') || this.get('rowSelectionMode');
+  actions: {
+    onSelectionToggled(event) {
+      let rowMeta = this.get('rowMeta');
+      let checkboxSelectionMode = this.get('checkboxSelectionMode') || this.get('rowSelectionMode');
 
-    if (rowMeta && checkboxSelectionMode === SELECT_MODE.MULTIPLE) {
-      let toggle = true;
-      let range = event.shiftKey;
+      if (rowMeta && checkboxSelectionMode === SELECT_MODE.MULTIPLE) {
+        let toggle = true;
+        let range = event.shiftKey;
 
-      rowMeta.select({ toggle, range });
-    } else if (rowMeta && checkboxSelectionMode === SELECT_MODE.SINGLE) {
-      rowMeta.select();
-    }
+        rowMeta.select({ toggle, range });
+      } else if (rowMeta && checkboxSelectionMode === SELECT_MODE.SINGLE) {
+        rowMeta.select();
+      }
 
-    this.sendFullAction('onSelect');
-  }
+      this.sendFullAction('onSelect');
+    },
 
-  @action
-  onCollapseToggled() {
-    let rowMeta = this.get('rowMeta');
-
-    rowMeta.toggleCollapse();
-
-    this.sendFullAction('onCollapse');
-  }
+    onCollapseToggled() {
+      let rowMeta = this.get('rowMeta');
+      rowMeta.toggleCollapse();
+      this.sendFullAction('onCollapse');
+    },
+  },
 
   click(event) {
     this.sendFullAction('onClick', { event });
-  }
+  },
 
   doubleClick(event) {
     this.sendFullAction('onDoubleClick', { event });
-  }
+  },
 
   sendFullAction(action, values = {}) {
     // If the action doesn't exist, it's not being used. Do nothing
@@ -181,5 +176,5 @@ export default class EmberTd extends BaseTableCell {
     });
 
     this.sendAction(action, values);
-  }
-}
+  },
+});
