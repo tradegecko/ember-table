a771d72583a17577be53ab59ba88cf5a5ea16c70
selvagsz <selvaganeshbsg@gmail.com>
2018-08-29 17:22:38 +0800

    dedecorate components/-private/row-wrapper.js

--- addon/components/-private/row-wrapper.js
+++ addon/components/-private/row-wrapper.js
@@ -4,129 +4,136 @@ import hbs from 'htmlbars-inline-precompile';
 import EmberObject, { get, set } from '@ember/object';
 import { A as emberA } from '@ember/array';
 
-import { tagName } from '@ember-decorators/component';
-import { argument } from '@ember-decorators/argument';
+// import { tagName } from '@ember-decorators/component';
+// import { argument } from '@ember-decorators/argument';
 
-import { computed } from '@ember-decorators/object';
+import { computed } from '@ember/object';
 
 import { objectAt } from '../../-private/utils/array';
-import { dynamicAlias } from '../../-private/utils/computed';
+import { dynamicAliasNew } from '../../-private/utils/computed';
 
-class CellWrapper extends EmberObject {
-  @dynamicAlias('rowValue', 'columnValue.valuePath')
-  cellValue;
+const CellWrapper = EmberObject.extend({
+  cellValue: dynamicAliasNew('rowValue', 'columnValue.valuePath'),
 
-  @computed('rowMeta', 'columnValue')
-  get cellMeta() {
-    let rowMeta = get(this, 'rowMeta');
-    let columnValue = get(this, 'columnValue');
+  cellMeta: computed('rowMeta', 'columnValue', {
+    get() {
+      let rowMeta = get(this, 'rowMeta');
+      let columnValue = get(this, 'columnValue');
 
-    if (!rowMeta._cellMetaCache.has(columnValue)) {
-      rowMeta._cellMetaCache.set(columnValue, EmberObject.create());
-    }
+      if (!rowMeta._cellMetaCache.has(columnValue)) {
+        rowMeta._cellMetaCache.set(columnValue, EmberObject.create());
+      }
 
-    return rowMeta._cellMetaCache.get(columnValue);
-  }
-}
+      return rowMeta._cellMetaCache.get(columnValue);
+    },
+  }),
+});
 
 const layout = hbs`{{yield api}}`;
 
-@tagName('')
-export default class RowWrapper extends Component {
-  layout = layout;
+export default Component.extend({
+  tagName: '',
+  layout,
 
-  @argument
-  rowValue;
+  // @argument
+  // rowValue;
 
-  @argument
-  columns;
+  // @argument
+  // columns;
 
-  @argument
-  columnMetaCache;
+  // @argument
+  // columnMetaCache;
 
-  @argument
-  rowMetaCache;
+  // @argument
+  // rowMetaCache;
 
-  @argument
-  canSelect;
+  // @argument
+  // canSelect;
 
-  @argument
-  rowSelectionMode;
+  // @argument
+  // rowSelectionMode;
 
-  @argument
-  checkboxSelectionMode;
+  // @argument
+  // checkboxSelectionMode;
 
-  _cells = emberA([]);
+  init() {
+    this._super(...arguments);
+    this._cells = emberA([]);
+  },
 
   destroy() {
     this._cells.forEach(cell => cell.destroy());
 
-    super.destroy(...arguments);
-  }
+    this._super(...arguments);
+  },
 
-  @computed('rowValue', 'rowMeta', 'cells', 'canSelect', 'rowSelectionMode')
-  get api() {
-    let rowValue = this.get('rowValue');
-    let rowMeta = this.get('rowMeta');
-    let cells = this.get('cells');
-    let canSelect = this.get('canSelect');
-    let rowSelectionMode = canSelect ? this.get('rowSelectionMode') : 'none';
+  api: computed('rowValue', 'rowMeta', 'cells', 'canSelect', 'rowSelectionMode', {
+    get() {
+      let rowValue = this.get('rowValue');
+      let rowMeta = this.get('rowMeta');
+      let cells = this.get('cells');
+      let canSelect = this.get('canSelect');
+      let rowSelectionMode = canSelect ? this.get('rowSelectionMode') : 'none';
 
-    return { rowValue, rowMeta, cells, rowSelectionMode };
-  }
+      return { rowValue, rowMeta, cells, rowSelectionMode };
+    },
+  }),
 
-  @computed('rowValue')
-  get rowMeta() {
-    let rowValue = this.get('rowValue');
-    let rowMetaCache = this.get('rowMetaCache');
+  rowMeta: computed('rowValue', {
+    get() {
+      let rowValue = this.get('rowValue');
+      let rowMetaCache = this.get('rowMetaCache');
 
-    return rowMetaCache.get(rowValue);
-  }
+      return rowMetaCache.get(rowValue);
+    },
+  }),
 
-  @computed(
+  cells: computed(
     'rowValue',
     'rowMeta',
     'columns.[]',
     'canSelect',
     'checkboxSelectionMode',
-    'rowSelectionMode'
-  )
-  get cells() {
-    let columns = this.get('columns');
-    let numColumns = get(columns, 'length');
+    'rowSelectionMode',
+    {
+      get() {
+        let columns = this.get('columns');
+        let numColumns = get(columns, 'length');
 
-    let rowValue = this.get('rowValue');
-    let rowMeta = this.get('rowMeta');
-    let canSelect = this.get('canSelect');
-    let checkboxSelectionMode = canSelect ? this.get('checkboxSelectionMode') : 'none';
-    let rowSelectionMode = canSelect ? this.get('rowSelectionMode') : 'none';
+        let rowValue = this.get('rowValue');
+        let rowMeta = this.get('rowMeta');
+        let canSelect = this.get('canSelect');
+        let checkboxSelectionMode = canSelect ? this.get('checkboxSelectionMode') : 'none';
+        let rowSelectionMode = canSelect ? this.get('rowSelectionMode') : 'none';
 
-    let { _cells } = this;
+        let { _cells } = this;
 
-    if (numColumns !== _cells.length) {
-      while (_cells.length < numColumns) {
-        _cells.pushObject(CellWrapper.create());
-      }
+        if (numColumns !== _cells.length) {
+          while (_cells.length < numColumns) {
+            _cells.pushObject(CellWrapper.create());
+          }
 
-      while (_cells.length > numColumns) {
-        _cells.popObject().destroy();
-      }
+          while (_cells.length > numColumns) {
+            _cells.popObject().destroy();
+          }
+        }
+
+        _cells.forEach((cell, i) => {
+          let columnValue = objectAt(columns, i);
+          let columnMeta = this.get('columnMetaCache').get(columnValue);
+
+          set(cell, 'checkboxSelectionMode', checkboxSelectionMode);
+          set(cell, 'rowSelectionMode', rowSelectionMode);
+
+          set(cell, 'columnValue', columnValue);
+          set(cell, 'columnMeta', columnMeta);
+
+          set(cell, 'rowValue', rowValue);
+          set(cell, 'rowMeta', rowMeta);
+        });
+
+        return _cells;
+      },
     }
-
-    _cells.forEach((cell, i) => {
-      let columnValue = objectAt(columns, i);
-      let columnMeta = this.get('columnMetaCache').get(columnValue);
-
-      set(cell, 'checkboxSelectionMode', checkboxSelectionMode);
-      set(cell, 'rowSelectionMode', rowSelectionMode);
-
-      set(cell, 'columnValue', columnValue);
-      set(cell, 'columnMeta', columnMeta);
-
-      set(cell, 'rowValue', rowValue);
-      set(cell, 'rowMeta', rowMeta);
-    });
-
-    return _cells;
-  }
-}
+  ),
+});
