045e81c23e333d0acaedadaad2df94ff62fb7b3c
selvagsz <selvaganeshbsg@gmail.com>
2019-07-23 11:07:58 +0800

    Replace the bower imports with npm in ember-table legacy

--- index.js
+++ index.js
@@ -6,15 +6,16 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    app.import(app.bowerDirectory + '/antiscroll/antiscroll.js');
-    app.import(app.bowerDirectory + '/antiscroll/antiscroll.css');
-    app.import(app.bowerDirectory + '/jquery-ui/jquery-ui.js');
-    app.import(app.bowerDirectory + '/jquery-mousewheel/jquery.mousewheel.js');
+    app.import('node_modules/antiscroll/antiscroll.js');
+    app.import('node_modules/antiscroll/antiscroll.css');
+    // We install jquery-custom-ui from /vendor
+    // app.import('node_modules/jquery-ui/jquery-ui.js');
+    app.import('node_modules/jquery-mousewheel/jquery.mousewheel.js');
   },
 
   afterInstall: function() {
-    this.addBowerPackageToProject('antiscroll');
-    this.addBowerPackageToProject('jquery-mousewheel');
-    this.addBowerPackageToProject('jquery-ui');
+    // this.addBowerPackageToProject('antiscroll');
+    // this.addBowerPackageToProject('jquery-mousewheel');
+    // this.addBowerPackageToProject('jquery-ui');
   }
 };
